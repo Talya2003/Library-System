eb7b4b13cf0c71a27ef2edd3904dc63911929b66
Talya Kazayof <106309652+Talya2003@users.noreply.github.com>
2023-11-27 10:59:47 +0200

    Main.java

--- Main.java
+++ Main.java
@@ -4,31 +4,62 @@ import java.awt.event.*;
 
 public class Main implements ActionListener {
 
+    public JFrame frame;
+    public JMenuBar menu_bar;
+    public JMenu home_menu, books_menu, members_menu, help_menu, settings_menu;
+    public JMenuItem search_book_by_name, search_book_by_code, search_member_by_name, search_member_by_code;
+
     public Main() {
-        JFrame frame = new JFrame();
+        frame = new JFrame();
+
+        //closes the software when the X of the frame is clicked.
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500 , 500);
+
+        //sets the screen size to be the screen size of the computer
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setSize(screenSize);
+
+        frame.setTitle("Library System");
+
+        //sets the icon to the logo of the library
+        Image icon = new ImageIcon("C://Users//Azrieli//IntelliJIDEAProjects//Library").getImage();
+        frame.setIconImage(icon);
+
         frame.setLayout(new FlowLayout());
 
-        JMenuBar menu_bar = new JMenuBar();
+        menu_bar = new JMenuBar();
 
-        JMenu home_menu = new JMenu("Home");
-        JMenu books_menu = new JMenu("Books");
-        JMenu members_menu = new JMenu("Members");
-        JMenu help_menu = new JMenu("Help");
-        JMenu settings_menu = new JMenu("Settings");
+        home_menu = new JMenu("Home");
+        books_menu = new JMenu("Books");
+        members_menu = new JMenu("Members");
+        help_menu = new JMenu("Help");
+        settings_menu = new JMenu("Settings");
 
-        JMenuItem search_book_by_name = new JMenuItem("Search book by name");
-        JMenuItem search_book_by_code = new JMenuItem("Search book by code");
+        search_book_by_name = new JMenuItem("Search book by name");
+        search_book_by_code = new JMenuItem("Search book by code");
+
+        search_member_by_name = new JMenuItem("Search member by name");
+        search_member_by_code = new JMenuItem("Search member by code");
 
         search_book_by_name.addActionListener(this);
         search_book_by_code.addActionListener(this);
 
-        JMenuItem search_member_by_name = new JMenuItem("Search member by name");
-        JMenuItem search_member_by_code = new JMenuItem("Search member by code");
+        search_member_by_name.addActionListener(this);
+        search_member_by_code.addActionListener(this);
 
-        search_member_by_name.addActionListener(this);
-        search_member_by_name.addActionListener(this);
+        //add Shortcuts
+        //click on Alt + the letter (VK_?) and it will be opened
+        home_menu.setMnemonic(KeyEvent.VK_H);
+        books_menu.setMnemonic(KeyEvent.VK_B);
+        members_menu.setMnemonic(KeyEvent.VK_M);
+        help_menu.setMnemonic(KeyEvent.VK_H);
+        settings_menu.setMnemonic(KeyEvent.VK_S);
+
+        search_book_by_name.setMnemonic(KeyEvent.VK_S);
+        search_book_by_name.setMnemonic(KeyEvent.VK_S);
+
+        search_book_by_name.setMnemonic(KeyEvent.VK_M);
+        search_book_by_name.setMnemonic(KeyEvent.VK_M);
 
         books_menu.add(search_book_by_name);
         books_menu.add(search_book_by_code);
@@ -50,7 +81,24 @@ public class Main implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == search_book_by_name) {
-            System.out.println("you search a book");
+            System.out.println("you search a book by name");
+        }
+
+        if (e.getSource() == search_book_by_code) {
+            System.out.println("you search a book by code");
+        }
+
+        if (e.getSource() == search_member_by_name) {
+            System.out.println("you search a member by name");
+        }
+
+        if (e.getSource() == search_member_by_code) {
+            System.out.println("you search a member by code");
         }
     }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+    }
+
 }
