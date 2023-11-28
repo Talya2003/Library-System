0891a9964df85de18ddeec7e4c85e177037b5fa7
Talya Kazayof <106309652+Talya2003@users.noreply.github.com>
2023-11-28 18:02:55 +0200

    Main.java

--- Main.java
+++ Main.java
@@ -9,8 +9,7 @@ public class Main implements ActionListener {
     public JMenu home_menu, books_menu, members_menu, help_menu, settings_menu;
     public JMenuItem search_book_by_name, search_book_by_code, search_member_by_name, search_member_by_code;
     public ImageIcon logo , search_img , library_img;
-    public JLabel welcome_label , background_label;
-//    public JPanel panel;
+    public JLabel welcome_label , content_label , background_label;
 
 
     public Main() {
@@ -42,6 +41,7 @@ public class Main implements ActionListener {
         //sets the background color of the JMenu
         home_menu.setBackground(Color.decode("#F8EBDD"));
 
+
         search_book_by_name = new JMenuItem("Search book by name");
         search_book_by_code = new JMenuItem("Search book by code");
 
@@ -63,20 +63,26 @@ public class Main implements ActionListener {
         settings_menu.setMnemonic(KeyEvent.VK_S);
 
         home_menu.setForeground(Color.WHITE);
-        home_menu.setFont(new Font("Calibri" , Font.PLAIN , 20));
+        home_menu.setFont(new Font("Calibri" , Font.BOLD , 14));
 
         books_menu.setForeground(Color.WHITE);
-        books_menu.setFont(new Font("Calibri" , Font.PLAIN , 20));
+        books_menu.setFont(new Font("Calibri" , Font.BOLD , 14));
 
         members_menu.setForeground(Color.WHITE);
-        members_menu.setFont(new Font("Calibri" , Font.PLAIN , 20));
+        members_menu.setFont(new Font("Calibri" , Font.BOLD , 14));
 
         help_menu.setForeground(Color.WHITE);
-        help_menu.setFont(new Font("Calibri" , Font.PLAIN , 20));
+        help_menu.setFont(new Font("Calibri" , Font.BOLD , 14));
 
         settings_menu.setForeground(Color.WHITE);
-        settings_menu.setFont(new Font("Calibri" , Font.PLAIN , 20));
-        
+        settings_menu.setFont(new Font("Calibri" , Font.BOLD , 14));
+
+        //aligns the JMenu to the center of the JMenuBar
+        home_menu.setHorizontalAlignment(SwingConstants.CENTER);
+        books_menu.setHorizontalAlignment(SwingConstants.CENTER);
+        members_menu.setHorizontalAlignment(SwingConstants.CENTER);
+        help_menu.setHorizontalAlignment(SwingConstants.CENTER);
+        settings_menu.setHorizontalAlignment(SwingConstants.CENTER);
 
         search_book_by_name.setMnemonic(KeyEvent.VK_S);
         search_book_by_name.setMnemonic(KeyEvent.VK_S);
@@ -116,49 +122,40 @@ public class Main implements ActionListener {
         background_label = new JLabel(library_img);
         background_label.setLayout(new BorderLayout());
 
-        //sets opacity to (50% --> 128 out of 255)
+        //sets opacity to 32% --> 80 out of 255
         background_label.setOpaque(true);
         background_label.setBackground(new Color(255, 255, 255, 80));
 
-        // Uncomment the relevant code for the welcome_label
         welcome_label = new JLabel("WELCOME");
         welcome_label.setForeground(Color.BLACK);
         welcome_label.setFont(new Font("Calibri", Font.BOLD, 100));
-
         // Set the border to create space from the top
         welcome_label.setBorder(BorderFactory.createEmptyBorder(100, 400, 0, 400));
-
-        // Add the JLabel to the background label
+        //adds the JLabel to the background label
         background_label.add(welcome_label, BorderLayout.NORTH);
 
 
+        //the content of the home page
+        String htmlContent = "<html><div><br/><br/><br/></div>"
+                + "<p style='font-size: 10px; color: #D4C5B6; font-family: 'Calibri' , sans-serif;'>Welcome to the library system! " +
+                "Our library was founded in 1948 with the birth of the state. Here you will feel at home. The good and calm atmosphere, " +
+                "the vintage design <br/>and the warm attitude. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut tempora assumenda " +
+                "voluptates, voluptas maiores beatae molestiae a corrupt similique <br/>magnam. Repellendus odit mollitia modi deserunt animi. " +
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut tempora assumenda voluptates, voluptas a maiores <br/>beatae molestiae " +
+                "a corrupt similique magnam. Repellendus and odit mollitia modi deserunt animi.</p> </html>";
+        //creates a label and put the content on the screen
+        content_label = new JLabel(htmlContent);
+        content_label.setBorder(BorderFactory.createEmptyBorder(-400, 100, 0, 0));
+        //add this to the background label
+        background_label.add(content_label);
 
-//        welcome_label = new JLabel("WELCOME TO OUR LIBRARY");
-//        welcome_label.setForeground(Color.decode("#F8EBDD"));
-
-        // Add the JLabel to the JMenu
-//        frame.add(welcome_label);
-
-
-
-//        welcome_label = new JLabel("Welcome to Friends Library!");
-//        welcome_label.setFont(new Font("Calibri", Font.BOLD, 60));
-//        welcome_label.setForeground(Color.decode("#F8EBDD"));
-//
-//        //creates a JPanel to hold the JLabel and sets its layout to the center of this frame
-//        panel = new JPanel(new GridBagLayout());
-//        panel.setBorder(BorderFactory.createEmptyBorder(150, 0, 0, 0));
-//        panel.add(welcome_label);
-//
-//        panel.add(welcome_label);
-//
-//        frame.getContentPane().add(background_label, BorderLayout.CENTER);
-//
-//        //sets the content pane of the frame to the background label
+        //sets the content pane of the frame to the background label
         frame.setContentPane(background_label);
 
         frame.setVisible(true);
     }
+
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
